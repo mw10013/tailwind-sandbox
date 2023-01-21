adcb2f83979457cddb7dc860dd724d142ad6b1f9
Michael Wu <mw10013@gmail.com>
2023-01-21 13:25:58 -0500

    convert

---
+++ apps/pkt-remix/app/components/Image.tsx
@@ -0,0 +1,11 @@
+export function Image({
+  priority,
+  unoptimized,
+  ...props
+}: React.ComponentPropsWithoutRef<"img"> & {
+  priority?: boolean;
+  unoptimized?: boolean;
+}) {
+  // eslint-disable-next-line jsx-a11y/alt-text
+  return <img {...props} />;
+}
