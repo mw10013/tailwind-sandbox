84b6d5b64d48bf94d13748741f3682c1ba8177cb
Michael Wu <mw10013@gmail.com>
2023-01-22 13:10:36 -0500

    add meta v2

--- apps/spl-remix/app/root.tsx
+++ apps/spl-remix/app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -9,40 +9,75 @@ import {
 } from "@remix-run/react";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 
-// export type ContextType = {};
+const modeScript = `
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
-// export const loader = (async () => {
-//   return json({
-//     data: "data",
-//   });
-// }) satisfies LoaderFunction;
+  updateMode()
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+  window.addEventListener('storage', updateModeWithoutTransitions)
 
+  function updateMode() {
+    let isSystemDarkMode = darkModeMediaQuery.matches
+    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode
+    }
+  }
+
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
+  }
+
+  function updateModeWithoutTransitions() {
+    disableTransitionsTemporarily()
+    updateMode()
+  }
+`;
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
 };
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Spl-Remix",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Spl-Remix" },
+    { name: "description", content: "Spotlight Demo" },
+  ];
+};
 
 export default function App() {
-  // const context: ContextType = {};
-
-  // <div> with min-h-full help register layout correctly.
-  // Mimics nextjs: #__next { min-height: 100%; }
   return (
-    <html lang="en">
+    <html className="h-full antialiased" lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          // href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`}
+          href="http://localhost:5001/rss/feed.xml"
+        />
+        <link
+          rel="alternate"
+          type="application/feed+json"
+          // href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
+          href="http://localhost:5001/rss/feed.json"
+        />
         <Meta />
         <Links />
       </head>
-      <body>
-        <div className="min-h-full">
-          {/* <Outlet context={context} /> */}
-          <Outlet />
-        </div>
+      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
